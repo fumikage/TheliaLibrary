f8a2fbec52efe01e57e9a9e6d98067712baf8f95
Théo Cherblanc <tcherblanc@openstudio.fr>
2024-12-17 11:23:31 +0100

    feat: #14 #17 improve product gallery and stick product image on scroll

--- templates/frontOffice/menuiserieblanc/components/Layout/ProductGallery/ProductGallery.js
+++ templates/frontOffice/menuiserieblanc/components/Layout/ProductGallery/ProductGallery.js
@@ -5,6 +5,14 @@ export default function ProductGallery() {
 
   if (!main || thumbs.length === 0) return null;
 
+  const thumbsSize = 6;
+  let firstThumbsId = 0;
+
+  const nextButton = document.querySelector('#nav-next');
+  const prevButton = document.querySelector('#nav-prev');
+
+  updateGallery();
+
   [...thumbs].forEach((img) => {
     img.addEventListener('click', (e) => {
       resetFocus();
@@ -17,7 +25,36 @@ export default function ProductGallery() {
     });
   });
 
+  nextButton.addEventListener('click', () => {
+    if (firstThumbsId + thumbsSize < thumbs.length) {
+      firstThumbsId++;
+      updateGallery();
+    }
+  });
+
+  prevButton.addEventListener('click', () => {
+    if (firstThumbsId > 0) {
+      firstThumbsId--;
+      updateGallery();
+    }
+  });
+
   function resetFocus() {
     [...thumbs].forEach((el) => el.parentNode.classList.remove('is-active'));
+  }
+
+  function updateGallery() {
+    thumbs.forEach((img, id) => {
+      img.parentNode.classList.toggle(
+        'hidden',
+        id < firstThumbsId || id >= firstThumbsId + thumbsSize
+      );
+    });
+
+    prevButton.classList.toggle('hidden', firstThumbsId === 0);
+    nextButton.classList.toggle(
+      'hidden',
+      firstThumbsId + thumbsSize >= thumbs.length
+    );
   }
 }
